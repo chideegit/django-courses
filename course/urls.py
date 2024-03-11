5e4a7f624bfbab24010f201d2b18f30bbd936a18
chidee <chidiebereohiri@gmail.com>
2024-03-11 12:48:50 +0100

    added urls.py file to course app

---
+++ course/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from .views import *
+
+urlpatterns = [
+    path('add-course/', add_course, name='add-course'), 
+    path('update-course/<int:pk>/', update_course, name='update-course'), 
+    path('all-courses/', all_courses, name='all-courses'), 
+    path('delete-course/<int:pk>/', delete_course, name='delete-course'), 
+    path('enrol-course/<int:pk>/', enrol_course, name='enrol-course'), 
+    path('all-enrolled-courses/', all_enrolled_courses, name='all-enrolled-courses')
+]
